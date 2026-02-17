c7931a1070dd8b069ded4556c9db1d2491ca59d8
pavel <pavelpetsevitch@gmail.com>
2026-02-18 01:16:27 +0200

    Some server Fixes

--- websocket-server.php
+++ websocket-server.php
@@ -9,9 +9,11 @@ $ws_worker->name = 'WebWorker';
 $ws_worker->count = 1;
 
 $ws_worker->onConnect = function($connection) use ($ws_worker) {
+    $ws_worker->connections[$connection->id] = $connection;
 };
 
 $ws_worker->onClose = function($connection) use ($ws_worker) {
+    unset($ws_worker->connections[$connection->id]);
 };
 
 $ws_worker->onWorkerStart = function($ws_worker)
@@ -19,9 +21,11 @@ $ws_worker->onWorkerStart = function($ws_worker)
     $inner_worker = new Worker("text://127.0.0.1:2346");
     $inner_worker->name = 'AjaxWorker';
 
-    $inner_worker->onMessage = function(TcpConnection $connection, $data) use ($ws_worker) {
+    $inner_worker->onMessage = function(TcpConnection $connection, $data) use ($ws_worker)
+    {
+        $clean_data = trim(data);
         foreach ($ws_worker->connections as $client_connection) {
-            $client_connection->send($data);
+            $client_connection->send($clean_data);
         }
         $connection->close();
     };
